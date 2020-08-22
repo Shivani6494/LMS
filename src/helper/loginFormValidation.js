46e039fcc9bb97f5113576f6b09d58472eb5a1f7
shivani-2020 <bhattshivani64@gmail.com>
2020-08-22 10:55:51 -0400

    commit

--- src/helper/loginFormValidation.js
+++ src/helper/loginFormValidation.js
@@ -1,10 +1,10 @@
 export default function loginFormValidation(values){
     const errors = {};
 
-    if (!values.email) {
-        errors.email = "User Name is required field."
-    } else if (!/\S+@\S+\.\S+/.test(values.email)){
-        errors.email = "User Name is not valid."
+    if (!values.userName) {
+        errors.userName = "User Name is required field."
+    } else if (!/\S+@\S+\.\S+/.test(values.userName)){
+        errors.userName = "User Name is not valid."
     }
 
     if (!values.password){
